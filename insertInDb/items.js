66f04a6cf3c74904bea743d6217b9a8205fc1006
V1NT4G3C0D3 <pragatibhattad1610@gmail.com>
2020-11-04 18:01:06 +0530

    add database insertion scripts

---
+++ insertInDb/items.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const Pool = require("pg").Pool;
+const fastcsv = require("fast-csv");
+
+let stream = fs.createReadStream("items.csv");
+let csvData = [];
+let csvStream = fastcsv
+  .parse()
+  .on("data", function(data) {
+    csvData.push(data);
+  })
+  .on("end", function() {
+    // remove the first line: header
+    csvData.shift();
+
+    // create a new connection to the database
+    const pool = new Pool({
+      host: "localhost",
+      user: "postgres",
+      database: "dbms_project",
+      password: "<password>",
+      port: 5432
+    });
+
+    const query =
+      "INSERT INTO FoodItems (itemId, restaurantID, itemName, itemDesc, Price) VALUES ($1, $2, $3, $4, $5, $6, $7)";
+
+    pool.connect((err, client, done) => {
+      if (err) throw err;
+
+      try {
+        csvData.forEach(row => {
+          client.query(query, row, (err, res) => {
+            if (err) {
+              console.log(err.stack);
+            } else {
+              console.log("inserted " + res.rowCount + " row:", row);
+            }
+          });
+        });
+      } finally {
+        done();
+      }
+    });
+  });
+
+stream.pipe(csvStream);
